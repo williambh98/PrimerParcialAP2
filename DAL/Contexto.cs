928245559b39088f82c4a9440fe1f313158cb525
williambh98 <42818093+williambh98@users.noreply.github.com>
2019-09-24 15:52:24 -0400

    Bll Lista

---
+++ DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Contexto : DbContext
+    {
+        //public DbSet<Pacientes> Pacientes { get; set; }
+
+        public Contexto() : base("ConStr")
+        {
+
+        }
+    }
+}
